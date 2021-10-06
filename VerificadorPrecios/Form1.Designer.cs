85bfd51a36d38d1487b3df828ee0bbd290ccad08
lucas <lucas@GRUB>
2021-10-06 04:01:32 -0700

    Agregar archivos de proyecto.

---
+++ VerificadorPrecios/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿
+namespace VerificadorPrecios
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.product_image = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.product_price = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.product_Name = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.product_image)).BeginInit();
+            this.SuspendLayout();
+ 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(53, 20);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(57, 52);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(308, 166);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(218, 121);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(162, 385);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(496, 29);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Coloque el código de barras por el sensor";
+
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(302, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 37);
+            this.label2.TabIndex = 8;
+ 
+            this.product_Name.AutoSize = true;
+            this.product_Name.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.product_Name.Location = new System.Drawing.Point(455, 85);
+            this.product_Name.Name = "product_Name";
+            this.product_Name.Size = new System.Drawing.Size(180, 29);
+            this.product_Name.TabIndex = 9;
+            this.product_Name.Text = "product_Name";
+ 
+            this.product_price.AutoSize = true;
+            this.product_price.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.product_price.Location = new System.Drawing.Point(455, 216);
+            this.product_price.Name = "product_price";
+            this.product_price.Size = new System.Drawing.Size(173, 29);
+            this.product_price.TabIndex = 4;
+            this.product_price.Text = "product_price";
+ 
+            this.product_image.Location = new System.Drawing.Point(146, 60);
+            this.product_image.Name = "product_image";
+            this.product_image.Size = new System.Drawing.Size(218, 240);
+            this.product_image.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.product_image.TabIndex = 2;
+            this.product_image.TabStop = false;
+
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+ 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(160)))), ((int)(((byte)(71)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.product_Name);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.product_price);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.product_image);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.product_image)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox product_image;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label product_price;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label product_Name;
+    }
+}
+
---
+++ VerificadorPrecios/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace VerificadorPrecios
+{
+    
+    public partial class Form1 : Form
+    {
+        private int tiempo = 0;
+        private String codigo = "";
+
+        private bool proFind = true;
+
+        private MySqlDataReader result;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Size = new System.Drawing.Size(Screen.PrimaryScreen.WorkingArea.Width / 2, 
+                Screen.PrimaryScreen.WorkingArea.Height / 2);
+            pictureBox1.Location = new Point(this.Width / 13 - pictureBox1.Width / 2,
+                this.Height / 12 - pictureBox1.Height + 30);
+            pictureBox2.Location = new Point(this.Width / 2 - pictureBox2.Width / 2,
+                this.Height / 2 + pictureBox2.Height - 260);
+            label1.Location = new Point(this.Width / 2 - label1.Width / 2,
+                this.Height / 2 + label1.Height + 60);
+            label2.Location = new Point(this.Width / 2 - label2.Width / 2,
+                20);
+            product_price.Visible = false;
+            product_image.Visible = false;
+            product_Name.Visible = false;    
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                try
+                {
+                    MySqlConnection server;
+                    server = new MySqlConnection("server=127.0.0.1; user=root; database=verificador_pr; SSL Mode=None;");
+                    server.Open();
+                    String queryString = "SELECT producto_codigo, producto_nombre, producto_precio, producto_imagen " +
+                        "FROM vp_productos WHERE producto_codigo = " + codigo + ";";
+                    MySqlCommand query = new MySqlCommand(queryString, server);
+                    result = query.ExecuteReader();
+
+                    if (result.HasRows)
+                    {
+                        pictureBox2.Image = global::VerificadorPrecios.Properties.Resources.gifload;
+                        pictureBox2.Size = new System.Drawing.Size(100, 100);
+                        pictureBox2.Location = new Point(this.Width / 2 - pictureBox2.Width / 2,
+                            this.Height / 2 - pictureBox2.Height + 40);
+                        pictureBox2.Visible = true;
+                        tiempo = 0;
+                        timer1.Enabled = true;
+                        label1.Visible = false;
+                        label2.Visible = false;
+                        pictureBox1.Visible = false;
+                        product_price.Visible = false;
+                        product_Name.Visible = false;
+                        product_image.Visible = false;
+                    }
+                    else
+                    {
+                        proFind = false;
+                        label1.Text = "Error al leer el código.\n     Intente otra vez";
+                        label1.Location = new Point(this.Width / 2 - label1.Width / 2,
+                            this.Height / 2 + label1.Height + 40);
+                        pictureBox2.Image = global::VerificadorPrecios.Properties.Resources.error_msg;
+                        pictureBox2.Size = new System.Drawing.Size(170, 170);
+                        pictureBox2.Location = new Point(this.Width / 2 - pictureBox2.Width / 2,
+                            this.Height / 2 + pictureBox2.Height - 300);
+                        pictureBox2.Visible = true;
+                        tiempo = 0;
+                        timer1.Enabled = true;
+                        label2.Visible = false;
+                        pictureBox1.Visible = false;
+                        product_price.Visible = false;
+                        product_Name.Visible = false;
+                        product_image.Visible = false;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString(), "Titulo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    throw;
+                }
+                codigo = "";
+            }
+            else
+            {
+                codigo += e.KeyChar;
+            }
+
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            tiempo++;
+            if (tiempo == 2 && proFind)
+            {
+                result.Read();
+                product_image.ImageLocation = result.GetString(3);
+                product_image.SizeMode = PictureBoxSizeMode.StretchImage;
+                product_image.Location = new Point((this.Width / 4) - (product_image.Width / 2),
+                    (this.Height / 3) - product_image.Height / 2 + 50);
+                product_image.Visible = true;
+                product_Name.Text = result.GetString(1);
+                product_Name.Location = new Point((this.Width / 2) - product_image.Width + 10, 
+                    (this.Height / 3 ) + product_image.Height - 50);
+                product_Name.Visible = true;
+                product_price.Text = "Precio: $" + result.GetString(2);
+                product_price.Location = new Point((this.Width / 2),
+                    (this.Height / 2) - product_price.Height);
+                product_price.Visible = true;
+                label1.Visible = false;
+                label2.Visible = false;
+                pictureBox2.Visible = false;
+            }
+            else if (tiempo == 5 && !proFind)
+            {
+                timer1.Enabled = false;
+                returnToOriginal();
+            }
+            if (tiempo == 8)
+            {
+                timer1.Enabled = false;
+                returnToOriginal();
+            }
+        }
+        private void returnToOriginal()
+        {
+            pictureBox2.Visible = true;
+            label1.Visible = true;
+            label1.Text = "Coloque el código de barras por el sensor";
+            label2.Visible = true;
+            label2.Text = "";
+            pictureBox1.Visible = true;
+            label1.Location = new Point(this.Width / 2 - label1.Width / 2,
+                this.Height / 2 + label1.Height + 60);
+            product_price.Visible = false;
+            product_Name.Visible = false;
+            pictureBox2.Size = new System.Drawing.Size(218, 121);
+            pictureBox2.Location = new Point(this.Width / 2 - pictureBox2.Width / 2,
+                this.Height / 2 + pictureBox2.Height - 260);
+            pictureBox2.Image = global::VerificadorPrecios.Properties.Resources.barcode_scan;
+            product_image.Visible = false;
+            product_image.ImageLocation = null;
+
+            proFind = true;
+        }
+    }
+}
